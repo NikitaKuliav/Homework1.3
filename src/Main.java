69a841277b5e0fface3c08180c46d439975db78b
Nikita <athlon64x2600@gmail.com>
2022-05-29 13:04:17 +0200

    Задание 1. Выведите в одну строку через пробел числа от 1 до 10, используя цикл while. На следующей строке выведите числа в обратном порядке от 10 до 1, используя оператор for. Для обоих циклов можно использовать как одну общую переменную, так и в каждом цикле свою.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задание 1
+
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+
+        System.out.println();
+
+        for (i = 10; i > 0; i--) {
+            System.out.print(i + " ");
+        }
     }
 }
